5011615354ccd3b1716ee5168b30235f4ba43ec5
LWK Sistemas <lwksistemas@gmail.com>
2026-01-21 10:57:39 -0300

    Corrigir URLs nos emails - usar login_page_url correto em vez de localhost

--- backend/enviar_email_harmonis.py
+++ backend/enviar_email_harmonis.py
@@ -22,7 +22,7 @@ Olá!
 Sua loja "{loja.nome}" foi criada com sucesso no nosso sistema!
 
 🔐 DADOS DE ACESSO:
-• URL de Login: http://localhost:3000{loja.login_page_url}
+• URL de Login: https://lwksistemas.com.br/loja/{loja.slug}
 • Usuário: {loja.owner.username}
 • Senha Provisória: {loja.senha_provisoria}
 

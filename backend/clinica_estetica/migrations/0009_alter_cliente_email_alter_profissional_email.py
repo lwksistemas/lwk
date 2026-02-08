96f34361a35b49921bb79feae9c444529f73536c
LWK Sistemas <lwksistemas@gmail.com>
2026-02-08 19:41:33 -0300

    v495: Corrige conflito de migrations - renomeia para 0009

--- backend/clinica_estetica/migrations/0009_alter_cliente_email_alter_profissional_email.py
+++ backend/clinica_estetica/migrations/0009_alter_cliente_email_alter_profissional_email.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clinica_estetica', '0001_initial'),
+        ('clinica_estetica', '0008_adicionar_sistema_financeiro'),
     ]
 
     operations = [

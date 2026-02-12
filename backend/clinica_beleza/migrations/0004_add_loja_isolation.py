b3be0059f0f244284460479d3fbcdf3c3cd36818
LWK Sistemas <lwksistemas@gmail.com>
2026-02-12 20:24:16 -0300

    fix: Renumerar migration de isolamento para 0004 (v583)

--- backend/clinica_beleza/migrations/0004_add_loja_isolation.py
+++ backend/clinica_beleza/migrations/0004_add_loja_isolation.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('clinica_beleza', '0001_initial'),
+        ('clinica_beleza', '0003_add_payment_comissao'),
     ]
 
     operations = [

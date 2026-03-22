8570391d03d9d649eb87c16adcafe7e69f1f8fa6
LWK Sistemas <lwksistemas@gmail.com>
2026-03-22 18:08:46 -0300

    fix: Corrige import de VendedorUsuario

--- backend/superadmin/management/commands/check_vendedor_propostas.py
+++ backend/superadmin/management/commands/check_vendedor_propostas.py
@@ -1,6 +1,6 @@
 from django.core.management.base import BaseCommand
-from superadmin.models import Loja
-from crm_vendas.models import Proposta, VendedorUsuario
+from superadmin.models import Loja, VendedorUsuario
+from crm_vendas.models import Proposta
 
 
 class Command(BaseCommand):

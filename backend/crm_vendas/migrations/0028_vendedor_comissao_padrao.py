86354f2f7798ce9b46148a69378e8cf77f86c858
LWK Sistemas <lwksistemas@gmail.com>
2026-03-22 14:19:54 -0300

    fix: corrigir número da migration de comissão padrão

--- backend/crm_vendas/migrations/0028_vendedor_comissao_padrao.py
+++ backend/crm_vendas/migrations/0028_vendedor_comissao_padrao.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('crm_vendas', '0001_initial'),
+        ('crm_vendas', '0027_add_complete_company_data_to_conta'),
     ]
 
     operations = [
